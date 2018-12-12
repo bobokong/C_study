a7e07a3d639d861b4c968f50436304acbc469b0c
bobokong <42274181+bobokong@users.noreply.github.com>
2018-12-12 23:39:41 +0800

    Add files via upload

---
+++ test_17_6_1.cpp
@@ -0,0 +1,16 @@
+#include "stdio.h" 
+#include <string.h> 
+/********found********/  
+void fun(char *s, char *t) 
+{ 
+/********found********/  	
+	s=t;
+} 
+main() 
+{ 
+ 	char s[100],t[100];  	
+	printf("\nPlease enter string S:");  	
+	scanf("%s%s",s,t);  	
+	fun(s,t); 
+ 	printf("\nThe result is : %s\n",s); 
+} 
